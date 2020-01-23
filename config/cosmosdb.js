ccefbb11904d9ca5959ed9fdf9c37d7b6abb483c
thomas brown <tbrown@nascent.com>
2020-01-23 12:45:16 -0500

    fix modules

--- config/cosmosdb.js
+++ config/cosmosdb.js
@@ -5,13 +5,11 @@ const db = config.get('azureCosmo');
 const connectDB = async () => {
   try {
     await mongoose.connect(db, {
-      //   auth: { user: 'thomas.brown1125@gmail.com', password: 'dttbFU#25' },
       useNewUrlParser: true,
       useCreateIndex: true,
       useUnifiedTopology: true,
       useFindAndModify: false
     });
-
     console.log('MongoDB Connected...');
   } catch (err) {
     console.error(err.message);
